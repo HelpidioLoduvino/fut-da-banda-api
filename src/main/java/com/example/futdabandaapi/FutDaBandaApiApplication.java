ff8a4be2a3cadb57c1389a6cca3d408e5a6bb43d
HelpidioLoduvino <helpidiom@gmail.com>
2024-06-26 13:16:40 +0100

    core: Configurações iniciais do projecto

---
+++ src/main/java/com/example/futdabandaapi/FutDaBandaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.futdabandaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutDaBandaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutDaBandaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/futdabandaapi/FutDaBandaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futdabandaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutDaBandaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
