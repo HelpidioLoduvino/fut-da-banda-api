ac323784e2d26b94d26e4dc224c47da0f3f1d20a
HelpidioLoduvino <helpidiom@gmail.com>
2024-08-05 20:09:52 +0100

    UserDto date type correction

--- src/main/java/com/example/futdabandaapi/dtos/UserDto.java
+++ src/main/java/com/example/futdabandaapi/dtos/UserDto.java
@@ -1,7 +1,6 @@
 package com.example.futdabandaapi.dtos;
 
-import java.time.LocalDate;
 import java.util.Date;
 
-public record UserDto(Long id, String fullName, String email, String userRole, LocalDate createdAt) {
+public record UserDto(Long id, String fullName, String email, String userRole, Date createdAt) {
 }
